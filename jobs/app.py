1521408b26ba759767dbc0b6ad3285bdd59b9697
taylormade84 <taylormade84@gmail.com>
2019-05-28 22:36:57 -0700

    updated

--- jobs/app.py
+++ jobs/app.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 
 def open_connection():
     connection = getattr(g, '_connection', None)
-
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
